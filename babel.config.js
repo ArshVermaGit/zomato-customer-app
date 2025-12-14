31e18acd11dac31b3de6f2646a92622f4b2ac516
Arsh Verma <arshverma.dev@gmail.com>
2025-12-14 15:48:55 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    '@babel/plugin-transform-export-namespace-from',
     process.env.NODE_ENV !== 'test' ? 'react-native-reanimated/plugin' : null,
   ].filter(Boolean),
 };
