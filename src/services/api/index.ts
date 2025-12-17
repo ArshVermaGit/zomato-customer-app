30d70b2e23c4041f967c46271dda33ca3153b0be
Arsh Verma <arshverma.dev@gmail.com>
2025-12-17 13:49:16 +0530

    Update index.ts

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,9 +1,3 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
-import { axiosBaseQuery } from './axiosBaseQuery';
-
-// Split API definition
-export const api = createApi({
-    baseQuery: axiosBaseQuery({ baseUrl: '' }),
-    tagTypes: ['User', 'Restaurants', 'Orders', 'Cart'],
-    endpoints: () => ({}),
-});
+export * from './authApi';
+export * from './ordersApi';
+export * from './restaurantsApi';
